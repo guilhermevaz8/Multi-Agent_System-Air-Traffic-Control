60f33a8165ac6042a2b4fec8164a14d48e28b49b
Pedro Campião <pedro.campiao1@gmail.com>
2023-11-14 06:54:30 +0000

    aircraft_positions

--- main_work/interface.py
+++ main_work/interface.py
@@ -49,13 +49,13 @@ while True:
         # Ajustar as posições para o tamanho da tela
         pos_x = int(position[0] * screen_width / 40)
         pos_y = int(position[1] * screen_height / 30)
-        pygame.draw.circle(screen, aircraft_color, (pos_x, pos_y), 20)
+        pygame.draw.circle(screen, aircraft_color, (pos_x, pos_y), 10)
 
     # Desenhar os aeroportos
     for color,pos in airport_positions.items():
         pos_x = int(pos[0] * screen_width / 40)
         pos_y = int(pos[1] * screen_height / 30)
-        pygame.draw.rect(screen, color, (*[pos_x,pos_y], 20,20))
+        pygame.draw.rect(screen, color, (*[pos_x-10,pos_y-10], 20,20))
 
     pygame.display.flip()
     clock.tick(1)  # 30 FPS para uma visualização mais suave
