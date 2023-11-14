ce8685fd1b83f1f0031e701889c04063f8b473f4
Pedro Campião <pedro.campiao1@gmail.com>
2023-11-14 05:48:41 +0000

    yau

--- main_work/interface.py
+++ main_work/interface.py
@@ -1,6 +1,10 @@
 import pygame
 import sys
 import json
+from random import randint
+from math import dist
+
+DISTANCE_BET_AIRP = 15
 
 # Inicialização do Pygame
 pygame.init()
@@ -21,10 +25,32 @@ clock = pygame.time.Clock()
 # Configuração de cores
 background_color = (0, 105, 148)  # Azul
 aircraft_color = (0, 0, 0)       # Preto
-airport_color = (255, 255, 255)  # Branco
+airport_colors = ["RED","GREEN","YELLOW","MAGENTA","WHITE"]  # Branco
+
+airport_positions = {}
 
 # Posições dos aeroportos
-airport_positions = [(100, 100), (300, 500), (600, 300)]
+for i in range(5):
+    pos = (randint(2,38),randint(2,28))
+    if airport_positions == {}:
+         airport_positions[airport_colors[i]]=pos
+         continue
+    mindist=False
+    while not mindist:
+        flag=True
+        for key,tmp in airport_positions.items():
+            x=tmp[0]
+            y=tmp[1]
+            while dist(pos,(x,y))<=DISTANCE_BET_AIRP:
+                flag=False
+                pos = (randint(2,38),randint(2,28))
+        if flag:
+            mindist=True
+        else:
+            mindist=False
+    airport_positions[airport_colors[i]]=pos
+
+
 
 # Loop principal
 while True:
@@ -41,11 +67,13 @@ while True:
         # Ajustar as posições para o tamanho da tela
         pos_x = int(position[0] * screen_width / 1000)
         pos_y = int(position[1] * screen_height / 1000)
-        pygame.draw.circle(screen, aircraft_color, (pos_x, pos_y), 5)
+        pygame.draw.circle(screen, aircraft_color, (pos_x, pos_y), 10)
 
     # Desenhar os aeroportos
-    for pos in airport_positions:
-        pygame.draw.rect(screen, airport_color, (*pos, 30, 30))
+    for color,pos in airport_positions.items():
+        pos_x = int(pos[0] * screen_width / 40)
+        pos_y = int(pos[1] * screen_height / 30)
+        pygame.draw.rect(screen, color, (*[pos_x,pos_y], 20,20))
 
     pygame.display.flip()
     clock.tick(1)  # 30 FPS para uma visualização mais suave
