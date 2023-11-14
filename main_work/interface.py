149e2bbdf45f8bac6729f6348a65d51c7b2b9032
ricardo <ricardonunocamposcosta@gmail.com>
2023-11-14 15:54:12 +0000

    ASDFGHJ

--- main_work/interface.py
+++ main_work/interface.py
@@ -1,3 +1,4 @@
+
 import pygame
 import sys
 import json
@@ -11,11 +12,18 @@ def load_aircraft_positions():
             return json.load(file)
     except FileNotFoundError:
         return {}
+    
+def load_airport_positions():
+    try:
+        with open("airport_positions.json", "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return {}
 
 # Configuração da janela do Pygame
 screen_width, screen_height = 800, 600
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Air Traffic Control Simulation")
+pygame.display.set_caption("Air Traffic Control Simulatiqon")
 clock = pygame.time.Clock()
 
 # Configuração de cores
@@ -23,8 +31,7 @@ background_color = (0, 105, 148)  # Azul
 aircraft_color = (0, 0, 0)       # Preto
 airport_color = (255, 255, 255)  # Branco
 
-# Posições dos aeroportos
-airport_positions = [(100, 100), (300, 500), (600, 300)]
+airport_positions = load_airport_positions()
 
 # Loop principal
 while True:
@@ -34,18 +41,23 @@ while True:
             sys.exit()
 
     aircraft_positions = load_aircraft_positions()
+    print(aircraft_positions)
+
     screen.fill(background_color)
 
     # Desenhar as aeronaves
-    for aircraft_id, position in aircraft_positions.items():
+    for aircraft_id, position_aircraft in aircraft_positions.items():
         # Ajustar as posições para o tamanho da tela
-        pos_x = int(position[0] * screen_width / 1000)
-        pos_y = int(position[1] * screen_height / 1000)
-        pygame.draw.circle(screen, aircraft_color, (pos_x, pos_y), 5)
+        pos_x_aircraft = int(position_aircraft[0] * screen_width / 1000)
+        pos_y_aircraft = int(position_aircraft[1] * screen_height / 1000)
+        pygame.draw.circle(screen, aircraft_color, (pos_x_aircraft, pos_y_aircraft), 5)
 
     # Desenhar os aeroportos
-    for pos in airport_positions:
-        pygame.draw.rect(screen, airport_color, (*pos, 30, 30))
+    for position_airport in airport_positions:
+        # Ajustar as posições para o tamanho da tela
+        pos_x_airport = int(position_airport[0] * screen_width / 1000)
+        pos_y_airport = int(position_airport[1] * screen_height / 1000)
+        pygame.draw.circle(screen, airport_color, (pos_x_airport, pos_y_airport), 5)
 
     pygame.display.flip()
     clock.tick(1)  # 30 FPS para uma visualização mais suave
